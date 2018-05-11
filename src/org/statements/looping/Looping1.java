0aa2335528240f2ae02f5cb6bc1e2e19b23ff2f7
levs.blinnikovs <levs.blinnikovs@4finance.com>
2018-05-11 11:39:27 +0200

    looping 1 ex

--- src/org/statements/looping/Looping1.java
+++ src/org/statements/looping/Looping1.java
@@ -10,12 +10,26 @@ public class Looping1 {
         System.out.println("Please input two integers: ");
 
         int a = userInput.nextInt();
-        int d = userInput.nextInt();
+        int b = userInput.nextInt();
 
-        // TODO a / b     a % b using +, -, compare
+        // init default values
+        int divRes = 0;
+        int modRes = 0;
 
-
-        
-
+        if (a>=b) {
+            for (;a > 0;a -= b, divRes++) {
+                if (a < b) {
+                    modRes = a;
+                    break;
+                }
+            }
+        }
+        else {
+            divRes = 0; // empirical knowledge that integer division of a<b is 0
+            modRes = a;
+        }
+        System.out.println("Division result is " + divRes);
+        System.out.println("Modulo result is " + modRes);
     }
+
 }
