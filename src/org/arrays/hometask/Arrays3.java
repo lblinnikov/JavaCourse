961eb335bd3f550ec0439938bb12552f591a96ed
levs.blinnikovs <levs.blinnikovs@4finance.com>
2018-05-14 14:10:42 +0200

    arrays ex3

--- src/org/arrays/hometask/Arrays3.java
+++ src/org/arrays/hometask/Arrays3.java
@@ -24,10 +24,21 @@ public class Arrays3 {
             intArr[i] = userInput.nextInt();
         }
 
-        // manipulations
-        //
-        for (int i=(arrSize-1);i>=0;i--){
-            System.out.println(intArr[i]);
+        // create subset array with length K
+        int[] lengthSub;
+        lengthSub = new int[length];
+
+        double avg;
+        int sum;
+
+        for (int i=0;i<(arrSize-length+1);i++){
+            System.arraycopy(intArr, i, lengthSub, 0, length);
+            sum = 0;
+            for (int j=0;j<length;j++) {
+                sum = sum+lengthSub[j];
+            }
+            avg = sum/(length+0.0);
+            System.out.println("#" + (i + 1) + " set rolling average: " + avg);
         }
 
     }
