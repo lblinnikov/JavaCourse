99b661c783ebf59743f62f85f8a7e1aa0fff94e7
levs.blinnikovs <levs.blinnikovs@4finance.com>
2018-06-05 11:48:35 +0200

    array size fix

--- src/edu/fourfinance/javacourse/oop/objects/PostalAddress.java
+++ src/edu/fourfinance/javacourse/oop/objects/PostalAddress.java
@@ -22,7 +22,7 @@ public class PostalAddress {
     }
 
     public String[] getAddress() {
-        String[] address = new String[5];
+        String[] address = new String[3];
         address[0] = city;
         address[1] = street;
         address[2] = houseNumber;
