2445e7a17ec16b2c5c969957aa789a6d0b664e4a
levs.blinnikovs <levs.blinnikovs@4finance.com>
2018-06-06 17:03:48 +0200

    add run class

--- src/random/Figure.java
+++ src/random/Figure.java
@@ -5,30 +5,37 @@ import java.util.List;
 
 public interface Figure {
 
-    static Integer getPerimeter(List<Integer> sides) {
+    public Integer getPerimeter(List<Integer> sides);
+
+}
+
+class AnyFigure implements Figure {
+
+    @Override
+    public Integer getPerimeter(List<Integer> sides) {
         return sides.stream().mapToInt(Integer::intValue).sum();
     }
 
 }
 
-
 class Run {
-
     public static void main(String... arg) {
+
+        AnyFigure abstractFigure = new AnyFigure();
 
         List<Integer> sides = new ArrayList<>();
 
         sides.add(100);
-        sides.add(100);
-        sides.add(100);
+        sides.add(10);
+        sides.add(1);
 
-        int result = Figure.getPerimeter(sides);
+        int result = abstractFigure.getPerimeter(sides);
 
         System.out.println("Perimeter is "+ result);
 
     }
-
 }
+
 
 
 
