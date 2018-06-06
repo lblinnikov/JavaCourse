58a97e3a55610af32c86537ed38a6486e02c2d23
levs.blinnikovs <levs.blinnikovs@4finance.com>
2018-06-06 12:14:58 +0200

    fix all

---
+++ src/random/Figure.java
@@ -0,0 +1,39 @@
+package random;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Figure {
+
+    public Integer getPerimeter(List<Integer> sides);
+
+}
+
+class AnyFigure implements Figure {
+
+    @Override
+    public Integer getPerimeter(List<Integer> sides) {
+        return sides.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public static void main(String... arg) {
+
+        AnyFigure abstractFigure = new AnyFigure();
+
+        List<Integer> sides = new ArrayList<>();
+
+            sides.add(1);
+            sides.add(-1);
+            sides.add(0);
+
+        int result = abstractFigure.getPerimeter(sides);
+
+        System.out.println("Perimeter is "+ result);
+
+    }
+
+}
+
+
+
+
