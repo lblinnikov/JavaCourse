5b683899ede75caed079860f4249d113468e8ba3
levs.blinnikovs <levs.blinnikovs@4finance.com>
2018-06-06 17:49:43 +0200

    fix

--- src/random/Figure.java
+++ src/random/Figure.java
@@ -47,12 +47,32 @@ class Square extends AnyFigure {
     }
 }
 
+class Box extends AnyFigure {
+
+    public Box(List<Integer> sides) {
+        super(sides);
+        if (!sides.stream().allMatch(it -> it.equals(sides.get(0)))) {
+            throw new IllegalArgumentException("this is not proper " + getName().toLowerCase());
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "BOX";
+    }
+
+    @Override
+    public Integer getSidesNumber() {
+        return 4;
+    }
+}
+
 abstract class AnyFigure implements Figure {
     protected final List<Integer> sides;
 
     protected AnyFigure(List<Integer> sides) {
         if (sides.size() != getSidesNumber()) {
-            throw new IllegalArgumentException("this is not proper :" + getName());
+            throw new IllegalArgumentException("this is not proper " + getName().toLowerCase());
         }
         this.sides = sides;
     }
@@ -68,12 +88,20 @@ class Run {
     public static void main(String... arg) {
         List<Integer> triangleSides = new ArrayList<>();
 
+        List<Integer> boxSides = new ArrayList<>();
+
+        boxSides.add(1);
+        boxSides.add(1);
+        boxSides.add(1);
+        boxSides.add(1);
+
+        Figure box = new Box(boxSides);
+
         triangleSides.add(100);
         triangleSides.add(10);
         triangleSides.add(1);
 
         Figure triangle = new Triangle(triangleSides);
-
         List<Integer> squareSides = new ArrayList<>();
 
         squareSides.add(100);
@@ -81,10 +109,12 @@ class Run {
         squareSides.add(1);
         squareSides.add(1);
 
+
         Figure square = new Square(squareSides);
 
         System.out.println("Perimeter triangle is " + triangle.getPerimeter());
         System.out.println("Perimeter square is " + square.getPerimeter());
+        System.out.println("Perimeter box is " + box.getPerimeter());
     }
 }
 
