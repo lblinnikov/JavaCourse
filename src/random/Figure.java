58e15e2c7de33aca70a8f3129ab7e9eddb828692
levs.blinnikovs <levs.blinnikovs@4finance.com>
2018-06-06 17:36:23 +0200

    fix

--- src/random/Figure.java
+++ src/random/Figure.java
@@ -5,34 +5,86 @@ import java.util.List;
 
 public interface Figure {
 
-    public Integer getPerimeter(List<Integer> sides);
+    public String getName();
 
+    public Integer getPerimeter();
+
+    public Integer getSidesNumber();
 }
 
-class AnyFigure implements Figure {
 
-    @Override
-    public Integer getPerimeter(List<Integer> sides) {
-        return sides.stream().mapToInt(Integer::intValue).sum();
+class Triangle extends AnyFigure {
+
+    public Triangle(List<Integer> sides) {
+        super(sides);
     }
 
+    @Override
+    public String getName() {
+        return "TRIANGLE";
+    }
+
+    @Override
+    public Integer getSidesNumber() {
+        return 3;
+    }
 }
+
+class Square extends AnyFigure {
+
+    public Square(List<Integer> sides) {
+        super(sides);
+    }
+
+    @Override
+    public String getName() {
+        return "SQUARE";
+    }
+
+    @Override
+    public Integer getSidesNumber() {
+        return 4;
+    }
+}
+
+abstract class AnyFigure implements Figure {
+    protected final List<Integer> sides;
+
+    protected AnyFigure(List<Integer> sides) {
+        if (sides.size() != getSidesNumber()) {
+            throw new IllegalArgumentException("this is not proper :" + getName());
+        }
+        this.sides = sides;
+    }
+
+    @Override
+    public Integer getPerimeter() {
+        return sides.stream().mapToInt(Integer::intValue).sum();
+    }
+}
+
 
 class Run {
     public static void main(String... arg) {
+        List<Integer> triangleSides = new ArrayList<>();
 
-        AnyFigure abstractFigure = new AnyFigure();
+        triangleSides.add(100);
+        triangleSides.add(10);
+        triangleSides.add(1);
 
-        List<Integer> sides = new ArrayList<>();
+        Figure triangle = new Triangle(triangleSides);
 
-        sides.add(100);
-        sides.add(10);
-        sides.add(1);
+        List<Integer> squareSides = new ArrayList<>();
 
-        int result = abstractFigure.getPerimeter(sides);
+        squareSides.add(100);
+        squareSides.add(10);
+        squareSides.add(1);
+        squareSides.add(1);
 
-        System.out.println("Perimeter is "+ result);
+        Figure square = new Square(squareSides);
 
+        System.out.println("Perimeter triangle is " + triangle.getPerimeter());
+        System.out.println("Perimeter square is " + square.getPerimeter());
     }
 }
 
