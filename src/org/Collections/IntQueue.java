ddd49405c8446fb6e0228f646737446760c95fe9
levs.blinnikovs <levs.blinnikovs@4finance.com>
2018-07-16 16:50:54 +0200

    IntQueue

---
+++ src/org/Collections/IntQueue.java
@@ -0,0 +1,45 @@
+package org.Collections;
+
+public class IntQueue {
+
+    /**
+     * Pushes element onto this stack.
+     *
+     * @param element The element to push onto this stack.
+     */
+
+    public void push(int element) {}
+
+    /**
+     * Pops the last value pushed onto this stack.
+     */
+
+    public void pull() {}
+
+    /**
+     * Retrieve without removal
+     *
+     * @return element
+     */
+
+    public int peek() {
+        int element;
+        return element;
+    }
+
+    /**
+     * Returns size (element count) of the queue.
+     *
+     * @return size;
+     */
+
+    public int size() {
+        int size;
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+}
