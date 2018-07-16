a02082fb6efdeb39565dae3d1bb737b7bb0c5ad9
levs.blinnikovs <levs.blinnikovs@4finance.com>
2018-07-16 16:45:16 +0200

    IntStack add

---
+++ src/org/Collections/IntStack.java
@@ -0,0 +1,30 @@
+package org.Collections;
+
+public class IntStack {
+
+    /**
+     * Pushes element onto this stack.
+     *
+     * @param element The element to push onto this stack.
+     */
+
+    public void push(int element) {}
+
+    /**
+     * Pops the last value pushed onto this stack.
+     */
+
+    public void pop() {}
+
+    /**
+     * Retrieve without removal
+     * @return int
+     */
+
+    public int peek() {}
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+}
