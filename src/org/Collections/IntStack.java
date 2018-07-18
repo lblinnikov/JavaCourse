b7c3f6c60c6a8143b6713f27d6c67d525427c26f
levs.blinnikovs <levs.blinnikovs@4finance.com>
2018-07-18 16:49:37 +0200

    Stack implementation for array

--- src/org/Collections/IntStack.java
+++ src/org/Collections/IntStack.java
@@ -2,31 +2,70 @@ package org.Collections;
 
 public class IntStack {
 
+    Integer maxSize;
+    Integer top;
+    Integer arr[];
+
+    public IntStack(int size) {
+        maxSize = size;
+        arr = new Integer[maxSize];
+        top = 0;
+    }
+
     /**
      * Pushes element onto this stack.
      * @param element The element to push onto this stack.
      */
 
-    public void push(int element) {}
+    public void push(Integer element) {
+        if(top < maxSize) {
+            arr[top] = element;
+            top++;
+        }
+        else {
+            System.out.println("Stack overflow!");
+        }
+    }
 
     /**
      * Pops the last value pushed onto this stack.
      */
 
-    public void pop() {}
+    public Integer pop() {
+        if(this.isEmpty()) {
+            int temp = this.peek();
+            arr[top-1] = null;
+            top--;
+            return temp;
+        }
+        else {
+            return null;
+        }
+    }
 
     /**
      * Retrieve without removal
      * @return element
      */
 
-   /* public int peek() {
-        int element;
-        return element;
+    public Integer peek() {
+        if(top > 0){
+            return arr[top-1];
+        }
+        else return null;
     }
-*/
+
+    /**
+     * Returns if stack is empty
+     */
+
     public boolean isEmpty() {
-        return false;
+        if(top == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
 }
--- src/org/Collections/Run.java
+++ src/org/Collections/Run.java
@@ -5,9 +5,25 @@ public class Run {
     public static void main(String... arg) {
 
 
-        Node second = new Node(2, null);
+        IntStack myStack = new IntStack(4);
+        System.out.println(myStack.isEmpty());
+        myStack.push(1);
+        System.out.println(myStack.isEmpty());
+        System.out.println(myStack.peek());
+        myStack.push(2);
+        System.out.println(myStack.peek());
+        myStack.push(3);
+        System.out.println(myStack.peek());
+        myStack.push(4);
+        System.out.println(myStack.peek());
+        myStack.push(5);
 
-        Node front = new Node(1,second);
+        myStack.pop();
+        System.out.println(myStack.peek());
+
+
+
+
 
 
     }
