2d2e81eb091311e7e0634c49af3cf7daf4aee916
levs.blinnikovs <levs.blinnikovs@4finance.com>
2018-08-13 12:03:34 +0200

    Spin words from Codewars

---
+++ src/org/Collections/Advanced/SpinWords.java
@@ -0,0 +1,37 @@
+package org.Collections.Advanced;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SpinWords {
+
+    public String spinWords(String sentence) {
+        String separator = " ";
+        String word = "";
+        List<String> wordList = new ArrayList<>(Arrays.asList(sentence.split(separator)));
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < wordList.size(); i++) {
+                word = wordList.get(i);
+                if (word.length() >= 5) {
+                    word = new StringBuilder(word).reverse().toString();
+                    //wordList.set(i, word);
+                }
+            sb.append(word);
+            sb.append(separator);
+        }
+        sentence = sb.toString().trim();
+        System.out.println(sentence);
+        return sentence;
+    }
+
+    public static void main(String... arg) {
+
+        SpinWords spinWords = new SpinWords();
+
+        spinWords.spinWords("Testing this long unmodified string");
+
+    }
+
+}
